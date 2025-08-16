4071c7d68db048044acc64c21da23b6e1545ed57
kkris26 <krisnuartha09@gmail.com>
2025-08-16 11:20:25 +0800

    fix : wrap useSearchParams with suspense

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import SearchForm from "../form/search-form";
 import Logo from "../logo/logo";
 import MenuSideBar from "../sidebar/menu-side-bar";
 import MenuNav from "../menu/navigation-menu";
+import { Suspense } from "react";
 export default function Navbar() {
   return (
     <nav className=" w-full bg-neutral-950/80 fixed top-0 left-0 z-2 backdrop-blur-sm items-center flex justify-center py-2 border-b shadow">
@@ -13,7 +14,9 @@ export default function Navbar() {
         </div>
         <MenuNav className="sm:block hidden" />
         <div className="w-60">
-          <SearchForm />
+          <Suspense>
+            <SearchForm />
+          </Suspense>
         </div>
       </div>
     </nav>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { Suspense } from "react";
-
-import LoadingNews from "../loading";
 import SearchContent from "./search-content";
 
 
 export default function SearchPage() {
   return (
-    <Suspense fallback={<LoadingNews type="search" />}>
+    <Suspense >
       <SearchContent />
     </Suspense>
   );
